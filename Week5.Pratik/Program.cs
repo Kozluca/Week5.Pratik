0b04daee24a1f7285c2df545ca0ee2fbc2553a57
Kozluca <okozluca.1@gmail.com>
2024-10-25 23:44:57 +0300

    Proje dosyası ekle.

---
+++ Week5.Pratik/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5.Pratik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayılar = new int[10]; // 10 Elemanlı bir dizi oluşturduk
+            int number = 8; // for döngüsü ile rastgele  bir değer atamak için number adında int bir değer tanımladım
+            for (int i = 0; i < sayılar.Length; i++) // for döngüsü ile sayılar dizisine elemanları yazdırdık.
+            {
+                number = number + i;
+                sayılar[i] = number;   // sayılar dizisinin i. index ine number değerini atıyoruz
+            }
+            foreach (var item in sayılar)    // sayılar dizisindeki elemanları yazdırdık
+            {
+                Console.Write(item+",");
+            }
+            Console.WriteLine(" ");
+            Console.Write("Eklenecek Sayıyı gir:");
+
+            int yeni=Convert.ToInt32(Console.ReadLine());    // yeni eklenecek sayı için karsıdan bir sayı alıyoruz yeni adında
+            Array.Resize(ref sayılar, sayılar.Length+1);     // dizinin boyutunu büyüttük
+            sayılar[sayılar.Length-1] = yeni;                 // yeni eklenecek olan sayıyı dizinin son elemanı olarak atadık
+            Console.WriteLine("Yeni Dizi");
+            for (int i = 0; i<sayılar.Length; i++)            // dizinin yeni halini yazdırdık
+            {
+                Console.WriteLine(sayılar[i]);
+            }
+            Console.WriteLine("-----Büyükten Küçüğe sıra -----");         
+            Array.Sort(sayılar);      // diziyi küçükten büyüğe sıraladık
+            Array.Reverse(sayılar);   // diziyi ters çevirdik
+            foreach(var item in sayılar )     // diziyi ekrana yazdırdık
+            {
+                Console.Write(item+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week5.Pratik/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Week5.Pratik")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week5.Pratik")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ed2fbb30-bf8a-468e-afba-53f788d81895")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
